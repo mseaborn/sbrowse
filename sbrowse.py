01fcd581dcb88e80634cd3a3ac67026f96c0fba9
Mark Seaborn <mrs@mythic-beasts.com>
2008-08-07 19:29:02 +0100

    Add CGI support

--- sbrowse.py
+++ sbrowse.py
@@ -30,7 +30,8 @@ css_file = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                         "styles.css")
 
 def handle_request(environ, start_response):
-    path = environ["PATH_INFO"]
+    path = environ.get("PATH_INFO", "/")
+    url_root = environ["SCRIPT_NAME"]
     query_list = parse_query(environ["QUERY_STRING"])
     query = dict(query_list)
     host_url = "http://%s" % environ["HTTP_HOST"]
@@ -40,22 +41,22 @@ def handle_request(environ, start_response):
     if path == "/search":
         start_response("302 OK",
                        [("Location", "%s/sym/%s"
-                         % (host_url, query["sym"]))])
+                         % (url_root, query["sym"]))])
         return ()
     empty, elt, rest = path.split("/", 2)
     if elt == "sym":
         start_response("200 OK", [("Content-Type", "text/html")])
-        return sym_search(rest)
+        return sym_search(url_root, rest)
     elif elt == "file":
         filename = rest
         if (filename != "" and not filename.endswith("/") and
             os.path.isdir(filename)):
             start_response("302 OK",
-                           [("Location", "%s/file/%s/" % (host_url, filename))])
+                           [("Location", "%s/file/%s/" % (url_root, filename))])
             return ()
         else:
             start_response("200 OK", [("Content-Type", "text/html")])
-            return show_file_or_dir(filename)
+            return show_file_or_dir(url_root, filename)
     else:
         start_response("404 Not found", [("Content-Type", "text/html")])
         return ["404 Not found"]
@@ -77,7 +78,7 @@ def stylesheet():
     finally:
         fh.close()
 
-def sym_search_in_filenames(sym):
+def sym_search_in_filenames(url_root, sym):
     proc = subprocess.Popen(
         ["sh", "-c", """ find -not -name "*.pyc" """],
         stdout=subprocess.PIPE, bufsize=1024)
@@ -91,8 +92,8 @@ def sym_search_in_filenames(sym):
                     % (cgi.escape(filename[:match.start()]),
                        cgi.escape(match.group()),
                        cgi.escape(filename[match.end():])))
-            yield ("<a href='/file/%s'>%s</a>\n"
-                   % (filename, text))
+            yield ("<a href='%s/file/%s'>%s</a>\n"
+                   % (url_root, filename, text))
     yield "</pre>"
 
 
@@ -105,7 +106,7 @@ class SymSearch(object):
         self.syms_found = {}
         self.syms_found_ci = {}
 
-    def match_line(self, line):
+    def match_line(self, url_root, line):
         """Tells you whether the line matches and returns a formatted version
         of the line with the matches highlighted."""
         does_match = False
@@ -115,7 +116,7 @@ class SymSearch(object):
                 line_out.append("<strong>%s</strong>" % token)
                 does_match = True
             elif is_symbol:
-                line_out.append(link_token(token))
+                line_out.append(link_token(url_root, token))
                 if self.sym_regexp.search(token):
                     self.syms_found[token] = \
                         self.syms_found.get(token, 0) + 1
@@ -126,28 +127,28 @@ class SymSearch(object):
                 line_out.append(cgi.escape(token))
         return (does_match, line_out)
 
-    def match_file(self, filename):
+    def match_file(self, url_root, filename):
         fh = open(filename, "r")
         try:
             for line_no, line in enumerate(fh):
                 line = line.rstrip("\n\r")
                 # Regexp search is an optimisation: could be removed
                 if self.sym_regexp.search(line):
-                    does_match, line_out = self.match_line(line)
+                    does_match, line_out = self.match_line(url_root, line)
                     if does_match:
                         yield (line_no, line_out)
         finally:
             fh.close()
 
 
-def sym_search(sym):
+def sym_search(url_root, sym):
     for x in stylesheet():
         yield x
     yield output_tag([tag("title", "symbol: ", sym),
                       tagp("div", [("class", "box")],
-                           tag("div", breadcrumb_path("")),
-                           tag("div", search_form(sym)))])
-    for x in sym_search_in_filenames(sym):
+                           tag("div", breadcrumb_path(url_root, "")),
+                           tag("div", search_form(url_root, sym)))])
+    for x in sym_search_in_filenames(url_root, sym):
         yield x
     proc = subprocess.Popen(
         ["sh", "-c",
@@ -158,10 +159,11 @@ def sym_search(sym):
     yield "<pre class=code>"
     for pipe_line in proc.stdout:
         filename = pipe_line.rstrip("\n\r")
-        for line_no, line_out in matcher.match_file(filename):
-            yield ("<a href='/file/%(file)s#line%(line_no)i'>"
+        for line_no, line_out in matcher.match_file(url_root, filename):
+            yield ("<a href='%(root)s/file/%(file)s#line%(line_no)i'>"
                    "%(file)s:%(line_no)i</a>:"
-                   % {"file": filename,
+                   % {"root": url_root,
+                      "file": filename,
                       "line_no": line_no + 1})
             for x in line_out:
                 yield x
@@ -173,35 +175,35 @@ def sym_search(sym):
         yield "none"
     else:
         if len(matcher.syms_found) > 0:
-            yield output_tag(format_sym_list(matcher.syms_found))
+            yield output_tag(format_sym_list(url_root, matcher.syms_found))
         if len(matcher.syms_found_ci) > 0:
             yield "with case relaxed:\n"
-            yield output_tag(format_sym_list(matcher.syms_found_ci))
+            yield output_tag(format_sym_list(url_root, matcher.syms_found_ci))
 
-def format_sym_list(syms):
+def format_sym_list(url_root, syms):
     body = []
     for symbol, count in sorted(syms.iteritems()):
         body.append(tag("li", 
-                        tagp("a", [("href", "/sym/%s" % symbol)],
+                        tagp("a", [("href", "%s/sym/%s" % (url_root, symbol))],
                              symbol),
                         " (%i)" % count))
     return tag("ul", body)
 
-def show_file_or_dir(filename):
+def show_file_or_dir(url_root, filename):
     if os.path.isdir(fix_path(filename)):
-        return show_dir(filename)
+        return show_dir(url_root, filename)
     else:
-        return show_file(filename)
+        return show_file(url_root, filename)
 
-def show_file(filename):
+def show_file(url_root, filename):
     for x in stylesheet():
         yield x
     links = [tag("div", tagp("a", [("href", url)], name))
              for name, url in get_file_links(filename)]
     yield output_tag([tag("title", filename),
                       tagp("div", [("class", "box")],
-                           tag("div", breadcrumb_path(filename)),
-                           tag("div", search_form("")),
+                           tag("div", breadcrumb_path(url_root, filename)),
+                           tag("div", search_form(url_root, "")),
                            links)])
     fh = open(filename, "r")
     try:
@@ -210,21 +212,21 @@ def show_file(filename):
             yield "<a name='line%i'>" % (line_no + 1)
             for token, is_symbol in tokens(line):
                 if is_symbol:
-                    yield link_token(token)
+                    yield link_token(url_root, token)
                 else:
                     yield cgi.escape(token)
         yield "</pre>"
     finally:
         fh.close()
 
-def show_dir(path_orig):
+def show_dir(url_root, path_orig):
     path = fix_path(path_orig)
     for x in stylesheet():
         yield x
     yield output_tag([tag("title", path),
                       tagp("div", [("class", "box")],
-                           tag("div", breadcrumb_path(path_orig)),
-                           tag("div", search_form("")))])
+                           tag("div", breadcrumb_path(url_root, path_orig)),
+                           tag("div", search_form(url_root, "")))])
     def format_entry(leafname):
         pathname = os.path.join(path, leafname)
         st = os.stat(pathname)
@@ -236,7 +238,7 @@ def show_dir(path_orig):
                    tagp("td", [("class", "file-size")], size),
                    tagp("td", [("class", "file-name")],
                         tagp("a", [("href", leafname)], leafname)))
-    yield output_tag([breadcrumb_path(path),
+    yield output_tag([breadcrumb_path(url_root, path),
                       tagp("table", [("class", "dirlist")],
                            tag("tr",
                                tagp("th", [("class", "file-size")], "size"),
@@ -254,13 +256,13 @@ def fix_path(path):
     else:
         return path
 
-def search_form(default_sym):
+def search_form(url_root, default_sym):
     script = """
 window.onload = function () {
     document.getElementById("form_field").focus();
 }
 """
-    return tagp("form", [("action", "/search"),
+    return tagp("form", [("action", "%s/search" % url_root),
                          ("method", "get")],
                 tagp("input", [("id", "form_field"),
                                ("type", "text"),
@@ -271,13 +273,13 @@ window.onload = function () {
                      "Go"),
                 tagp("script", [("language", "javascript")], script))
 
-def breadcrumb_path(path):
-    crumbs = [tagp("a", [("href", "/file/")], "[top]")]
+def breadcrumb_path(url_root, path):
+    crumbs = [tagp("a", [("href", "%s/file/" % url_root)], "[top]")]
     path_got = ""
     for element in path.split("/"):
         path_got = os.path.join(path_got, element)
         crumbs.append(["/",
-                       tagp("a", [("href", "/file/%s" % path_got)],
+                       tagp("a", [("href", "%s/file/%s" % (url_root, path_got))],
                             cgi.escape(element))])
     return crumbs
 
@@ -294,8 +296,9 @@ def tokens(line):
             yield (line[i:], False)
             return
 
-def link_token(token):
-    return "<a href='/sym/%(sym)s'>%(sym)s</a>" % {"sym": token}
+def link_token(url_root, token):
+    return "<a href='%(root)s/sym/%(sym)s'>%(sym)s</a>" % {"root": url_root,
+                                                           "sym": token}
 
 
 def tag(tag, *body):
@@ -341,24 +344,30 @@ def get_file_links(filename):
 
 
 def main(args):
+    do_cgi = False
     port = 8000
     once = False
     while len(args) > 0:
         arg = args.pop(0)
         if arg == "--dir":
             os.chdir(args.pop(0))
+        elif arg == "--cgi":
+            do_cgi = True
         elif arg == "--once":
             once = True
         elif arg == "--port":
             port = int(args.pop(0))
         else:
             raise Exception("Unknown argument: %s" % arg)
-    httpd = wsgiref.simple_server.make_server('', port, handle_request)
-    print "Listening on port %i" % port
-    if once:
-        httpd.handle_request()
+    if do_cgi:
+        wsgiref.handlers.CGIHandler().run(handle_request)
     else:
-        httpd.serve_forever()
+        httpd = wsgiref.simple_server.make_server('', port, handle_request)
+        print "Listening on port %i" % port
+        if once:
+            httpd.handle_request()
+        else:
+            httpd.serve_forever()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
